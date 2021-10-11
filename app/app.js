794431e60e94da376b26ea2eab14d3dc7ea69d75
johnnyvincent <johnvincentmyers@gmail.com>
2021-10-10 20:11:30 -0400

    add changes to html, js selectors, and css

--- app/app.js
+++ app/app.js
@@ -5,5 +5,23 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  // when new game button is selected, run function which clears spaces and
+  // resets the game
+ $('#new-game').on('click', beginNewGame)
+ // when board spaces are selected, run function which places
+ // the choosers x or o on that space and checks to see if
+ // the game is over.
+ $('box1').on('click', selectSpace)
+ $('box2').on('click', selectSpace)
+ $('box3').on('click', selectSpace)
+ $('box4').on('click', selectSpace)
+ $('box5').on('click', selectSpace)
+ $('box6').on('click', selectSpace)
+ $('box7').on('click', selectSpace)
+ $('box8').on('click', selectSpace)
+ $('box9').on('click', selectSpace)
+
+ $('#sign-up').on('submit', authEvents.onSignUp)
+ $('#sign-in').on('submit', authEvents.onSignIn)
+
 })
